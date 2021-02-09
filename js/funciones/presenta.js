498098105fb03e0bcbf3ae523a7f759cec763a93
le <conexionbeta@yahoo.es>
2021-02-08 23:22:14 -0500

    reordenamiento de archivos carpetas

---
+++ js/funciones/presenta.js
@@ -0,0 +1,4 @@
+function presenta(nombre, apellido){
+    console.log('mi nombre es '+nombre+' y mi apellido es '+apellido)
+}
+presenta('sutanito', 'sutanosa')
---
+++ js/funciones/suma.js
@@ -0,0 +1,14 @@
+
+function sumar(x,z) {
+    return (x + z);
+  }
+  
+  var a = 2;
+  var b = -11;
+  console.log(sumar(a, b));
+
+/* 
+function sumar(x, z){
+    return x+z;
+}
+console.log(sumar(2,-7)) */
