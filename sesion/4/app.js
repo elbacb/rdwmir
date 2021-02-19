9f9475f3d1878b7cd7faa44c1b1286986b9974c2
le <conexionbeta@yahoo.es>
2021-02-19 05:35:46 +0200

    reubicacion local (anterior: repo incorrecto)

---
+++ sesion/4/app.js
@@ -0,0 +1,49 @@
+console.log('trabajando con node...');
+console.log('...igual...');
+console.log('...gracias');
+console.log('trabajando con node...'+' 4 veces');
+console.log('javascript'=='Javascript');
+console.log('el resultado de comparar 5 + 3 es: ' + (5>3));
+let nombre
+console.log('el tipo de dato es: ' + typeof nombre);
+
+const cinco= 5;
+console.log('el tipo de dato de 5 + 3 es: ' + typeof cinco);
+
+nombre='Estrada'
+console.log('el tipo de dato es: ' + typeof nombre);
+
+//plantilla literal (string)(comilla al revés)
+console.log(`      la edad del susodicho
+                     
+
+
+        no es ${cinco}
+        
+        ` );
+
+//expresiones e instrucciones
+const dividendo=10;
+const divisor=11;
+
+if (divisor==0)//expresiones
+
+//instrucciones
+{
+console.log(`no es posible`);
+}
+
+else{
+    //instrucciones
+console.log(`${dividendo}`/`${divisor}`);
+console.log(dividendo/divisor);
+}
+
+
+//operador ternario
+const prod1= 2;
+const prod2 = 8;
+const total = prod1 + prod2;
+const valor = total >= 5 ? total-0.2*total: total;
+console.log(valor)
+
