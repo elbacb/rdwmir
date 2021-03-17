4e1d6151ee22b0c12d1136e3c38e68cf5b063b48
le <conexionbeta@yahoo.es>
2021-03-17 22:46:44 +0200

    ajuste

---
+++ sesion/10/cryptojs/encriptar.js
@@ -0,0 +1,18 @@
+/* no usado en index, por ahora*/
+
+/* librería: */
+
+/*  AES Encryption
+Plain text encryption
+
+var CryptoJS = require("crypto-js");
+ 
+// Encrypt
+var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123').toString();
+ 
+// Decrypt
+var bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret key 123');
+var originalText = bytes.toString(CryptoJS.enc.Utf8);
+ 
+console.log(originalText); // 'my message'
+*/
